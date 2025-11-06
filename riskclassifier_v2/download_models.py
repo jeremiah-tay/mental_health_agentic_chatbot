651b7d3848b3e7fc2bce2ae785ca28d41007e8a4
rachel chun <144464912+Chxlz@users.noreply.github.com>
2025-11-06 18:38:30 +0800

    Update download_models.py

--- riskclassifier_v2/download_models.py
+++ riskclassifier_v2/download_models.py
@@ -1,8 +1,9 @@
 import os, zipfile, gdown
 
-# Google Drive link for fine-tuned models
-# https://drive.google.com/file/d/1Hhl-sMI-usslHBP1hE8vb8KLDPyS-2ZC/view?usp=sharing
-FILE_ID = "1Hhl-sMI-usslHBP1hE8vb8KLDPyS-2ZC"
+
+# gdrive link for finetuned models: https://drive.google.com/file/d/1YhGQ9TQNs3pbJkEjzxcUl2K16XS4W1MF/view?usp=sharing
+# gdrive file id for finetuned and trained model: 1YhGQ9TQNs3pbJkEjzxcUl2K16XS4W1MF/
+FILE_ID = "1YhGQ9TQNs3pbJkEjzxcUl2K16XS4W1MF"
 OUTPUT_ZIP = "saved_models.zip"
 DEST_DIR = "saved_models"
 URL = f"https://drive.google.com/uc?id={FILE_ID}"
@@ -12,9 +13,9 @@ def download_and_extract():
     print(f"Downloading models from Google Drive...")
     gdown.download(URL, OUTPUT_ZIP, quiet=False)
 
-    print("Extracting models into:", DEST_DIR)
+    print("Extracting models...")
     with zipfile.ZipFile(OUTPUT_ZIP, "r") as zip_ref:
-        zip_ref.extractall(DEST_DIR)  # <--- FIXED: extract inside saved_models/
+        zip_ref.extractall(".")
     os.remove(OUTPUT_ZIP)
 
     print(f"Models ready in: {os.path.abspath(DEST_DIR)}")
